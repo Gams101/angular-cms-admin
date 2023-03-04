c28b6302876b02d840398faadf39cc7669ebbf42
Gammalil Basallo <gams.basallo@gmail.com>
2023-03-04 08:48:51 +0800

    Remove environment files

--- src/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  domain: 'laravel-cms-web-and-api',
-  webUrl: 'http://laravel-cms-web-and-api.test',
-  apiUrl: 'http://laravel-cms-web-and-api.test/api',
-};
