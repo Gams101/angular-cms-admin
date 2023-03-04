1700e5f2fb880794ef6517bb0f251a30b29f243b
Gammalil Basallo <gams.basallo@gmail.com>
2023-03-04 08:43:07 +0800

    Add environment variables

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  domain: '',
+  webUrl: '',
+  apiUrl: '',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  domain: 'laravel-cms-web-and-api',
+  webUrl: 'http://laravel-cms-web-and-api.test',
+  apiUrl: 'http://laravel-cms-web-and-api.test/api',
+};
